dc06ae866f95b591e466bf7054ad04ae9697b5e1
yd0144 <68176109+yd0144@users.noreply.github.com>
2023-09-17 05:29:01 +0000

    basic data types

---
+++ 01_basic/datatype.js
@@ -0,0 +1,8 @@
+//basic data types
+//alert() is not used in this because we are using node.
+let a=10            //number data type
+let b="yogesh"      //string data type
+let isLogIn=false   //boolean data type
+let c               //undefined data type 
+let d=null          //object data type for null and null is standalone value
+console.log(typeof isLogIn);
