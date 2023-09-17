cf89cf3411897635d76ce02b4ed20811165a64b1
yd0144 <68176109+yd0144@users.noreply.github.com>
2023-09-17 04:03:01 +0000

    variable with notes

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -1,2 +1,12 @@
-var a="yogesh"
-console.log("hello"+" "+a)
+let a="yogesh"
+var b=11
+const pi=3.14
+c=34
+console.table([a,b,pi,c])
+/* variable is noting but giving a name to memeory location.
+Here we are declaring and assiging values to variable .if we are not assigned any value to variable then it will take as undefined.
+We can declare variable in 2 ways one is using let and another one is var but var is not prefer to use because it does not have block and functional scope and 
+another thing with var is that we can redeclare same variable with var but with let we cannot do it. let is perfer to use.
+ we can assign value to variable without let or without var but it is not good practice
+ */
+//above program we used table method this is good to use when printing many variables in same time it will dispaly table with index and value column
