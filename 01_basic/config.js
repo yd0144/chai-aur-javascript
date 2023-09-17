0bc3c6a829286b1b6e369a5d143f2745555c9ea8
yd0144 <68176109+yd0144@users.noreply.github.com>
2023-09-17 03:40:17 +0000

    adding config notes

---
+++ 01_basic/config.js
@@ -0,0 +1,8 @@
+console.log("Hello folks")
+/*Here we are configuaring a virtual setup for javascript with github we create github 
+account and create one repository and in that repo go in code and their go in codespace .
+it will open virtual machine with code editor like visual studio code  in that go to 
+command palett using shortcut ctrl+shift+p and type container and go inside add dev container 
+config files and inside this go into create new configuration we can modify existing configuration as well but 
+we are using 1st time so we are creating nwe config and inside that
+install any thing we want after that rebulid that machine.*/
