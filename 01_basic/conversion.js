7e3bda5b4b7debef1a7b7a48ad218b205328c453
yd0144 <68176109+yd0144@users.noreply.github.com>
2023-09-17 07:43:11 +0000

    conversion is done

---
+++ 01_basic/conversion.js
@@ -0,0 +1,22 @@
+//Number is used to convert in number
+//String is used to convert in string
+//Boolean is used to convert in boolen
+let a="11a"
+let b=Number(a)
+//console.log(typeof a);     //here it is in string
+//console.log(typeof b);     //here it is converted into number but when we want actual value then it will give NaN(not a number)
+//console.log(b)             //here it will show NaN
+
+let isLogIn=1
+let change=Boolean(isLogIn)
+//console.log(typeof change);
+//console.log(change);        //where we can use 1 for true and 0 for false
+let x=""  
+let z="yogesh"
+let y=Boolean(x)
+let h=Boolean(z)
+console.log(typeof y);
+console.log(typeof h);
+console.log(y);        //if we take empty string and change it to boolean then it`s boolean value is false and take any string can convert it to boolean then it`s` boolean value is true
+console.log(h);
+ 
