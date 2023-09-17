8ff3e9d9f7b5665f94acafa911f2e9c360f059c5
yd0144 <68176109+yd0144@users.noreply.github.com>
2023-09-17 08:48:15 +0000

    prefix and postfix  is done with example

---
+++ 01_basic/postpre.js
@@ -0,0 +1,10 @@
+//For postfix
+// let a=10
+// let b=a++         //Here we 1st assign value to b so it will take b as 10 and after that it will increment and that value is stored in a
+// console.log(a);   
+// console.log(b);
+
+let c=20
+let d=++c        //Here we 1st increment value of c and then assign to d and that value is stored in c also so value in c and d is same 
+console.log(c);
+console.log(d);
