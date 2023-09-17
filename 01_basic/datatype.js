d26aca2c10a1ec6ead6ce1d2519be249c5c66a19
yd0144 <68176109+yd0144@users.noreply.github.com>
2023-09-17 05:54:50 +0000

    use strict

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -3,7 +3,7 @@
 //alert() is not used in this because we are using node.
 let a=11           //number data type
 let b="yogesh"      //string data type
-let isLogIn=false   //boolean data type
+let isLogIn=true  //boolean data type
 let c               //undefined data type 
 let d=null          //object data type for null and null is standalone value
 console.log(typeof isLogIn);
