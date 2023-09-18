a4a4729f3336c01b6979d1898e0e1af640594888
yd0144 <68176109+yd0144@users.noreply.github.com>
2023-09-18 09:09:56 +0000

    datatypes summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,24 @@
+// Premitive data types
+// Number String Boolean Null Undefined Symbol BigInt
+//Non-primitive data types
+//array object function
+
+// const names=["yogesh","prashant","hitesh"]    //This is simple array example
+// console.log(names)
+
+// const obj={
+//     nam:"yogesh",
+//     age:24,
+//     kam:"ASE",
+//     salary:25000,
+//     isActive:"Yes"
+
+// }                  //This is example of simple object we can take any type
+//                    //of value in object but it is in key:value pair
+// console.log(obj);
+
+// let fun=function()
+// {
+//     console.log("Hello i am function calling");   //This is example of simple function
+// }
+// console.log(fun);
